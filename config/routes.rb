7ee157bd632ed6880f2d94c20757346cabc30fad
Bhoopendra singh <bhoopendra2010singh@gmail.com>
2025-01-17 17:30:16 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  namespace :api do
+    get 'get_token', to: 'urls#get_token'
+    resources :urls, only: [:create, :show]
+  end
 
 end
 
